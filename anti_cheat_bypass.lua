9bec448c027c4bf193e8293e7096371c58cbc5eb
JulianS-random <juliansalvato17548@gmail.com>
2025-11-08 13:52:20 -0600

    Refactor string encryption and decryption functions

--- anti_cheat_bypass.lua
+++ anti_cheat_bypass.lua
@@ -1,46 +1,42 @@
+-- Simple Anti-Cheat Bypass (String Encryption)
+
 local function encryptString(str)  
-    local encrypted = ""  
-    for i = 1, #str do  
-        encrypted = encrypted .. string.char(string.byte(str, i) + 5) -- Add 5 to each character's ASCII value  
-    end  
-    return encrypted  
+  local encrypted = ""  
+  for i = 1, #str do  
+    encrypted = encrypted .. string.char(string.byte(str, i) + 5)  
+  end  
+  return encrypted  
 end
 
 local function decryptString(str)  
-    local decrypted = ""  
-    for i = 1, #str do  
-        decrypted = decrypted .. string.char(string.byte(str, i) - 5) -- Subtract 5 from each character's ASCII value  
-    end  
-    return decrypted  
+  local decrypted = ""  
+  for i = 1, #str do  
+    decrypted = decrypted .. string.char(string.byte(str, i) - 5)  
+  end  
+  return decrypted  
 end
 
 local encryptedScript = encryptString([[  
+-- The round start detector script goes here.  
 local function detectRoundStart()  
-    local loadingScreen = game.Workspace:FindFirstChild("LoadingScreen")  
-    if loadingScreen then  
-        print("Round start detected (Loading Screen)!")  
-        return true  
-    end
+  -- Placeholder Round Start Detection  
+  local loadingScreen = game.Workspace:FindFirstChild("LoadingScreen")  
+  if loadingScreen then  
+    print("Round start detected!")  
+    return true  
+  end
 
-    local roundStateAddress = 0xYourRoundStateAddressHere  
-    local roundState = krnl.readMemory(roundStateAddress, "int")
-
-    if roundState == 1 then  
-        print("Round start detected (Memory)!")  
-        return true  
-    end
-
-    return false  
+  return false  
 end
 
 while true do  
-    if detectRoundStart() then  
-        print("New round started!")  
-        break  
-    end  
-    wait(1)  
+  if detectRoundStart() then  
+    print("New round started!")  
+    break  
+  end  
+  wait(1)  
 end  
 ]])
 
 local decryptedScript = decryptString(encryptedScript)  
-loadstring(decryptedScript)() -- Execute the decrypted script  
+loadstring(decryptedScript)()  
